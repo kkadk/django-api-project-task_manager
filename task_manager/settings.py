ccc5e15e0430786d23938374f0aef5f959c75eac
kkadk <125792968+kkadk@users.noreply.github.com>
2025-03-08 09:39:53 +0545

    initialized .env variables

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q4cv8^8ys7gc#5#ew=6h&&wt^-^au5ry5dq!n!a6_wek)i)_&('
+import os
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

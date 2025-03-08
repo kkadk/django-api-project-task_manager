8b1ef161eb32c17550dc58c83773146660a3c23d
kkadk <125792968+kkadk@users.noreply.github.com>
2025-03-08 10:52:13 +0545

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
+
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
